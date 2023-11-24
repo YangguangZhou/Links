ff3b8c90625b55fe961188b41c691f313ee97bed
Jerry Zhou <61733195+YangguangZhou@users.noreply.github.com>
2023-11-24 23:54:38 +0800

    Update script.js

--- go/script.js
+++ go/script.js
@@ -3,7 +3,36 @@ let urls = localStorage.getItem('urls');
 let lastUpdate = localStorage.getItem('lastUpdate');
 let index = null;
 
+window.onload = function () {
+  window.setTimeout(url, 0);
+};
+
 function url() {
+  if (names && urls && lastUpdate && Date.now() - lastUpdate < 24 * 60 * 60 * 1000) {
+    names = JSON.parse(names);
+    urls = JSON.parse(urls);
+  } else {
+    fetch('../member.md')
+      .then(response => response.text())
+      .then(data => {
+        const lines = data.split('\n');
+        names = [];
+        urls = [];
+        for (let i = 4; i < lines.length; i++) {
+          if (lines[i] === '') break;
+          const cells = lines[i].split('|');
+          names.push(cells[2].trim());
+          urls.push(cells[3].trim());
+        }
+        localStorage.setItem('names', JSON.stringify(names));
+        localStorage.setItem('urls', JSON.stringify(urls));
+        localStorage.setItem('lastUpdate', Date.now());
+      });
+  }
+  window.setTimeout(links, 3000);
+}
+
+function links() {
   index = Math.floor(Math.random() * urls.length);
   if (document.referrer) {
     const origin = new URL(document.referrer).origin;
@@ -18,36 +47,8 @@ function url() {
   document.getElementById("WebsiteUrl").innerHTML = urls[index];
   document.getElementById("visitors").src = "https://visitor-badge.laobi.icu/badge?page_id=links-" + urls[index];
   console.log(index + ":" + urls[index]);
-}
-
-function links() {
   urls = urls
     .filter((url) => url)
     .map((url) => url);
-  window.open(urls[index], '_blank');
+  window.location = urls[index];
 }
-
-if (names && urls && lastUpdate && Date.now() - lastUpdate < 24 * 60 * 60 * 1000) {
-  names = JSON.parse(names);
-  urls = JSON.parse(urls);
-} else {
-  fetch('../member.md')
-    .then(response => response.text())
-    .then(data => {
-      const lines = data.split('\n');
-      names = [];
-      urls = [];
-      for (let i = 4; i < lines.length; i++) {
-        if (lines[i] === '') break;
-        const cells = lines[i].split('|');
-        names.push(cells[2].trim());
-        urls.push(cells[3].trim());
-      }
-      localStorage.setItem('names', JSON.stringify(names));
-      localStorage.setItem('urls', JSON.stringify(urls));
-      localStorage.setItem('lastUpdate', Date.now());
-    });
-}
-
-window.setTimeout(url, 0);
-window.setTimeout(links, 3000);
